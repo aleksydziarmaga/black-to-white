1fe646f1a062f79fadf0efe9c2d66a99a17cb9f3
Aleksy Dziarmaga <redron90@gmail.com>
2018-03-08 21:42:31 +0100

    Add solution

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Board from './components/Board';
+import Button from './components/Button';
+
 import BOARD from './models/board.model';
 import changeTilesColor from './utils/changeTilesColor';
 import createSteps from './utils/createSteps';
@@ -10,15 +12,34 @@ import createBoard from './utils/createBoard';
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
-    this.onClick = this.onClick.bind(this)
+    this.state = {
+      solution: this.props.steps
+    }
+    this.updateSolution = this.updateSolution.bind(this);    
+    this.onTileClick = this.onTileClick.bind(this);
+    this.showSolution = this.showSolution.bind(this);
   }
+  updateSolution(steps, index) {
+    steps.push(index);
+    this.setState({solution: steps})
+  }
+  onTileClick(x, y, board) {
+    const index = board.findIndex((el) => (el.x === x && el.y === y))
+    this.updateSolution(this.state.solution, index)
 
-  onClick(x, y, board) {
     const newBoard = changeTilesColor(x, y, board);
+    if (newBoard[index].solution === true)
+      newBoard[index].solution = false
     this.setState({ board: newBoard})
   }
-
+  showSolution() {
+    const solutionBoard = this.state.board;
+    const solution = this.state.solution;
+    for (let i = 0; i < solution.length; i++) {
+      solutionBoard[solution[i]].solution = true;
+    }
+    this.setState({board: solutionBoard});
+  }
   componentDidMount() {
     const cloneBoard = BOARD.map((e) => Object.assign({}, e))
     const newBoard = createBoard(cloneBoard, this.props.steps);
@@ -29,7 +50,8 @@ class App extends Component {
     return (
       <div className="app"> 
         <h1>Black to White</h1>
-        { this.state.board && <Board board={this.state.board} onClick={this.onClick} />}
+        { this.state.board && <Board board={this.state.board} onClick={this.onTileClick} />}
+        <Button text="Solution" onClick={this.showSolution} />
       </div>
     )
   }
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import cn from 'classnames';
 
 const Tile = ({tile, onClick, board}) => {
-    const className = cn('tile', {fill: !tile.empty});
+    const className = cn('tile', {fill: !tile.empty}, {'help-tile': tile.solution});
     return <div onClick={() => onClick(tile.x, tile.y, board)} className={className} />
 };
 
